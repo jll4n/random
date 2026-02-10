221b6c91ffcbc3b9622e555025519c3be2fcfac6
jll4n <julianpoulainpro@gmail.com>
2026-02-10 18:28:15 +0000

    initial issue

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("jll4n");
+MODULE_DESCRIPTION("Random gen num");
+
+static int __init random_init(void) {
+    printk(KERN_INFO "Random module loaded\n");
+    return 0;
+}
+
+static void __exit random_exit(void) {
+    printk(KERN_INFO "Random module unloaded\n");
+}
+
+int random_function(int ts){
+    int frac = 1/(ts);
+    int num = frac*frac;
+    int result = 0;
+    if (num % 2 == 0){
+        result = num/2;
+    } else {
+        result = num*3 + 1;
+    }
+    return result;
+}
+
+int random_gen_num(int min, int max){
+    ktime_t timestamp = ktime_get();
+    int ns = ktime_to_ns(timestamp);
+    return ns;
+}
